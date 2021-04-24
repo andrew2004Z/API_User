d2e7ff110cddd4a5a1e63e8758a230c85103f015
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-24 17:32:20 +0300

    Add get_users

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import flask
 from flask import jsonify, request
 from . import db_session
 from .users import User
+
+blueprint = flask.Blueprint(
+    'users_api',
+    __name__,
+    template_folder='templates'
+)
+
+
+@blueprint.route('/api/users', methods=['GET'])
+def get_users():
+    db = db_session.create_session()
+    return jsonify({'users': [item.to_dict(only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'hashed_password', 'modified', 'city_from')) for item in db.query(User).all()]})
