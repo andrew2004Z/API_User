5e3fa03ac1c95bad70795687f24bd718d318db22
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-24 18:06:39 +0300

    Add functions get_one_user

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ blueprint = flask.Blueprint(
 def get_users():
     db = db_session.create_session()
     return jsonify({'users': [item.to_dict(only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'hashed_password', 'modified', 'city_from')) for item in db.query(User).all()]})
+
+
+@blueprint.route('/api/users/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    db = db_session.create_session()
+    if not db.query(User).get(user_id):
+        return jsonify({'error': 'Not found'})
+    else:
+        return jsonify({'users': db.query(User).get(user_id).to_dict(only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email', 'hashed_password', 'modified', 'city_from'))})
